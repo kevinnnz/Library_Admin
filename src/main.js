c328da469e7c8b1da06d64525455816e95831df2
Kevin Zaworski <zaworskk@sheridancollege.ca>
2019-12-04 16:20:43 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,14 +8,7 @@ Vue.config.productionTip = false
 let app = ''
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBssP-9YfPwEYhSkp5UWl1c2m7g40oJfto",
-  authDomain: "kevinnnz.firebaseapp.com",
-  databaseURL: "https://kevinnnz.firebaseio.com",
-  projectId: "kevinnnz",
-  storageBucket: "kevinnnz.appspot.com",
-  messagingSenderId: "440965300084",
-  appId: "1:440965300084:web:4a9012f444bd2bb7a17621",
-  measurementId: "G-K6009ETC7D"
+  // YOUR CONFIG SETTINGS HERE
 }
 
 // Initialize Firebase
